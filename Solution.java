57d1e8a4640b20eb14af49afa2d944cf590f6309
Dima Smirnov <smirnovvonrims@gmail.com>
2021-03-17 22:37:51 +0300

    MVC Pattern Task

---
+++ Solution.java
@@ -0,0 +1,30 @@
+package com.javarush.task.task36.task3608;
+
+import com.javarush.task.task36.task3608.controller.Controller;
+import com.javarush.task.task36.task3608.model.MainModel;
+import com.javarush.task.task36.task3608.model.Model;
+import com.javarush.task.task36.task3608.view.EditUserView;
+import com.javarush.task.task36.task3608.view.UsersView;
+
+public class Solution {
+    public static void main(String[] args) {
+        Model model = new MainModel();
+
+        UsersView usersView = new UsersView();
+        Controller controller = new Controller();
+        EditUserView editUserView = new EditUserView();
+
+        controller.setModel(model);
+        controller.setUsersView(usersView);
+        controller.setEditUserView(editUserView);
+
+        usersView.setController(controller);
+        editUserView.setController(controller);
+
+        usersView.fireEventShowAllUsers();
+        usersView.fireEventOpenUserEditForm(126L);
+        editUserView.fireEventUserDeleted(124L);
+        editUserView.fireEventUserChanged("", 123, 1);
+        usersView.fireEventShowDeletedUsers();
+    }
+}
---
+++ view/EditUserView.java
@@ -0,0 +1,28 @@
+package com.javarush.task.task36.task3608.view;
+
+import com.javarush.task.task36.task3608.controller.Controller;
+import com.javarush.task.task36.task3608.model.ModelData;
+
+public class EditUserView implements View {
+    private Controller controller;
+
+    @Override
+    public void setController(Controller controller) {
+        this.controller = controller;
+    }
+
+    @Override
+    public void refresh(ModelData modelData) {
+        System.out.print("\nUser to be edited:");
+        System.out.print("\n\t" + modelData.getActiveUser());
+        System.out.print("\n===================================================");
+    }
+
+    public void fireEventUserDeleted(long id) {
+        controller.onUserDelete(id);
+    }
+
+    public void fireEventUserChanged(String name, long id, int level) {
+        controller.onUserChange(name, id, level);
+    }
+}
